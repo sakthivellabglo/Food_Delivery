b86bc6fd824a9571567917009ace613f962cc7cb
marilabglo <mariyammallabglo@gmail.com>
2022-12-26 12:18:58 +0530

    add jwt

--- delivery/settings.py
+++ delivery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 
@@ -178,3 +179,13 @@ ALLOWED_HOSTS = ['*']
 # ##python3 manage.py runserver 0.0.0.0:7000 http://192.168.1.77:7000/
 
 #192.168.1.77:7000
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
+}
--- delivery/urls.py
+++ delivery/urls.py
@@ -18,11 +18,18 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('delivery_app.urls')),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
       
 ]
 if settings.DEBUG:
